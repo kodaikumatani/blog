2096fb351c2a4140c1d0a65ae6f2143b3d3cd8be
kodaikumatani <koudai.aco5423@gmail.com>
2021-11-22 15:28:52 +0900

    Add pagination

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,8 @@ class Post extends Model
 	{
 		return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 	}
+	public function getPaginateByLimit(int $limit_count = 10)
+	{
+		return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+	}
 }
