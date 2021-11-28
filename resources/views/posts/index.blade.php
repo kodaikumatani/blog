c0c29e189968f70115e1b5d4929af3cbfd4ec64f
kodaikumatani <koudai.aco5423@gmail.com>
2021-11-28 05:20:49 +0000

    Deleting a blog post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,12 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-	[<a href='/posts/create'>create</a>]
+        <button type=“button” onclick="location.href='/posts/create'">create</button>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-			<h2 class='title'>
-    				<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-			</h2>
+                    <h2 class='title'></h2>
+                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                 </div>
             @endforeach
         </div>
